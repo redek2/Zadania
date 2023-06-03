fd79fb98926ce6b7efa7584d06c11a193412ec3c
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-03 16:09:54 +0200

    Add files via upload

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,25 @@
+﻿public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c;
+
+        Console.WriteLine("Podaj a:");
+        a = inputValue();
+        Console.WriteLine("Podaj b:");
+        b = inputValue();
+        Console.WriteLine("Podaj c:");
+        c = inputValue();
+        Console.WriteLine("Średnia arytmetyczna wynosi:");
+        double sredniaArytmetyczna;
+
+        sredniaArytmetyczna = (a + b + c) / 3;
+        Console.WriteLine(sredniaArytmetyczna);
+    }
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
