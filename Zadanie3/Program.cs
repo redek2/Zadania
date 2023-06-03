173126803d56d34494af1f177e4ead4142cc4acc
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-03 17:05:45 +0200

    Add files via upload

---
+++ Zadanie3/Program.cs
@@ -0,0 +1,24 @@
+﻿public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double w,h,bmi;
+
+        Console.WriteLine("Wyznaczanie BMI");
+        Console.WriteLine("Podaj swoją wagę i wzrost:");
+        Console.WriteLine("Waga w kilogramach:");
+        w = inputValue();
+        Console.WriteLine("Wzrost w metrach:");
+        h=inputValue();
+        bmi = w / Math.Pow(h, 2);
+        Console.WriteLine("Twoje BMI wynosi::");
+        
+        Console.WriteLine(bmi);
+    }
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
