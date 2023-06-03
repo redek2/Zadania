656910ecab94fb20750bdd7df215719bb1af5e15
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-03 16:49:46 +0200

    Add files via upload

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,22 @@
+﻿public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double p,o,r;
+
+        Console.WriteLine("Podaj pole koła:");
+        p = inputValue();
+        r = Math.Sqrt(p / Math.PI);
+        o = 2 * Math.PI * r;
+
+        Console.WriteLine("Obwód tego koła wynosi:");
+
+        Console.WriteLine(o);
+    }
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
