0c95280fb0a35efcb69bb5d7f965409dd8779d0a
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-03 17:53:06 +0200

    Add files via upload

---
+++ Zadanie4/Program.cs
@@ -0,0 +1,34 @@
+﻿public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int a, b, c, d, x, y;
+
+        Console.WriteLine("Wpisz a:");
+        a = (int)inputValue();
+        Console.WriteLine("Wpisz b:");
+        b = (int)inputValue();
+        Console.WriteLine("Wpisz c:");
+        c = (int)inputValue();
+        Console.WriteLine("Wpisz d:");
+        d = (int)inputValue();
+
+        if (b == 0 || d == 0)
+        {
+            Console.WriteLine("Brak rozwiązań");
+        }
+        else
+        {
+            x = ((a * d) + (b * c));
+            y = (b * d);
+            Console.WriteLine("x=" + x);
+            Console.WriteLine("y=" + y);
+        }
+    }
+    private static double inputValue()
+    {
+        int result;
+        while (!int.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
