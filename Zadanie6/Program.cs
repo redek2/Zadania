b3dd93c7127a1dad55facf30652e56b317eae7ad
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-03 20:19:50 +0200

    Add files via upload

---
+++ Zadanie6/Program.cs
@@ -0,0 +1,19 @@
+﻿public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c;
+        Console.WriteLine("Sprawdź czy trójkąt o podanych bokach a,b,c jest równoboczny");
+        Console.WriteLine("Podaj a:");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj b:");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj c:");
+        c = double.Parse(Console.ReadLine());
+
+        if (a == b & b == c)
+            Console.WriteLine("Trójkąt o podanych bokach jest równoboczny");
+        else 
+            Console.WriteLine("Trójkąt o podanych bokach nie jest równoboczny");
+    }
+}
