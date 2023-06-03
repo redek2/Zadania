1599d7e22e2652abd2db3c038e671169d3f2cb11
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-03 19:19:06 +0200

    Add files via upload

---
+++ Zadanie5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Transactions;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b;
+        Console.WriteLine("Podaj a");
+        a=double.Parse(Console.ReadLine());
+        Console.WriteLine("Podaj b:");
+        b=double.Parse(Console.ReadLine());
+        if (a == b) Console.WriteLine("Liczba "+a+" jest równa liczbie "+b);
+        else if (a > b) Console.WriteLine("Liczba " + a + " jest większa od liczby " + b);
+        else Console.WriteLine("Liczba " + b + " jest większa od liczby " + a);
+    }
+}
