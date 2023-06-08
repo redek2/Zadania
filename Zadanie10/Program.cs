420b613a0a7e0cf197b6f6bb667397a7c89dfad9
redek2 <128227165+redek2@users.noreply.github.com>
2023-06-08 03:07:38 +0200

    Add files via upload

---
+++ Zadanie10/Program.cs
@@ -0,0 +1,19 @@
+﻿public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        int a, n;
+        a = 0;
+        Console.WriteLine("Podaj liczbę naturalną:");
+        n = int.Parse(Console.ReadLine());
+        n =OstatniaCyfra(a,n);
+        Console.WriteLine("\nOstatnia cyfra Twojej liczby to: " + n);
+    }
+    
+    public static int OstatniaCyfra(int a, int n)
+    {
+        a = n % 10;
+
+        return a;
+    }
+}
